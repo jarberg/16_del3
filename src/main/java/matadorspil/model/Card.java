8b8cbb326451ce9380f93e76cb9a133a02c30bdb
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-15 15:32:12 +0100

    Added proper randomness test to DieTest Removed misinformed code conflicting with team design choices and VCS etiquette.

--- src/main/java/matadorspil/model/Card.java
+++ src/main/java/matadorspil/model/Card.java
@@ -2,20 +2,4 @@ package matadorspil.model;
 
 public class Card {
 
-    private String name;
-    private int effektID;
-
-    Card(){
-
-    }
-
-    Card(String name, int effektID){
-        this.name = name;
-        this.effektID = effektID;
-
-    }
-
-    public String getName(){ return name; }
-
-    public int getEffektID(){ return effektID; }
 }
--- src/main/java/matadorspil/model/ChanceCard.java
+++ src/main/java/matadorspil/model/ChanceCard.java
@@ -1,4 +1,23 @@
 package matadorspil.model;
 
 public class ChanceCard {
+
+    private String name;
+    private int effectID;
+
+    ChanceCard(){
+    }
+
+    ChanceCard(String name, int effektID){
+        this.name = name;
+        this.effectID = effektID;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getEffectID(){
+        return this.effectID;
+    }
 }
