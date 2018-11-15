e538767800816018dea7dcd0e465281d70730454
Rasmus <razmyz99@gmail.com>
2018-11-15 14:54:33 +0100

    Reorganized code in Die class

--- src/main/java/matadorspil/model/Die.java
+++ src/main/java/matadorspil/model/Die.java
@@ -1,11 +1,13 @@
 package matadorspil.model;
 
 public class Die {
+
 private int value;
-private void roll(){
-    value = 1 + (int)(Math.random() * 6);
-}
-public int getValue(){
-    return value;
-}
+
+    private void roll(){
+        value = 1 + (int)(Math.random() * 6);
+    }
+    public int getValue(){
+        return value;
+    }
 }
