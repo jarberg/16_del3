c8aca9864bbdac69b299bc42aa7381f3b25b8ff9
jdkramhoft <s175445@student.dtu.dk>
2018-11-18 12:15:24 +0100

    Refactored color handling to seperate class. Changed GUI_Player array to arraylist implementation.

--- src/main/java/monopoly/application/Main.java
+++ src/main/java/monopoly/application/Main.java
@@ -1,8 +1,18 @@
 package monopoly.application;
 
+import monopoly.view.MonopolyView;
+
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        MonopolyView view = new MonopolyView();
+        view.showEmptyGUI();
+        view.addPlayer(view.getPlayerName("name"),view.getUserColor("color"));
+        view.addPlayer(view.getPlayerName("name"),view.getUserColor("color"));
+        view.addPlayer(view.getPlayerName("name"),view.getUserColor("color"));
+        view.addPlayer(view.getPlayerName("name"),view.getUserColor("color"));
+        view.addPlayer(view.getPlayerName("name"),view.getUserColor("color"));
     }
 
 }
--- src/main/java/monopoly/view/MonopolyView.java
+++ src/main/java/monopoly/view/MonopolyView.java
@@ -13,13 +13,16 @@ public class MonopolyView {
     private String[] languages;
     private String[] playerAmounts;
     private ArrayList<Color> colors;
-    private GUI_Player[] guiPlayers;
+    private ArrayList<GUI_Player> guiPlayers;
 
 
     public MonopolyView(){
+        //Consider de-hardcoding language choices and player numbers.
         this.languages = new String[]{"Danish", "English"};
         this.playerAmounts = new String[]{"2", "3", "4"};
         this.colors = new ArrayList<>();
+        this.guiPlayers = new ArrayList<>();
+        //Consider de-hardcoding colors.
         this.colors.add(Color.BLUE);
         this.colors.add(Color.ORANGE);
         this.colors.add(Color.WHITE);
@@ -38,14 +41,13 @@ public class MonopolyView {
     }
 
     public void addPlayer(String name, Color color){
+        //The uniqueness of names should be controlled in controller (player object names also need uniqueness)
         GUI_Player player = new GUI_Player(name);
         player.getCar().setPrimaryColor(color);
+        this.guiPlayers.add(player);
         this.gui.addPlayer(player);
     }
 
-    public void initlializeGUIplayerArray(int size){
-        this.guiPlayers = new GUI_Player[size];
-    }
 
     private GUI_Player getGUIplayerByName(String name){
         GUI_Player player = null;
@@ -81,68 +83,18 @@ public class MonopolyView {
     }
 
     public Color getUserColor(String message){
-        String[] colorChoices = colorsAsStringArray();
-        String userColorString = getUserChoice(message, colorChoices);
-        Color colorChosen = stringToColor(userColorString);
-        removeColorFromChoices(colorChosen);
-        return colorChosen;
+        ColorHandler colorHandler = new ColorHandler(gui, colors);
+        return colorHandler.getUserColor(message);
     }
+
+
 
     public int getUserAge(String message){
         int userAgeInput = this.gui.getUserInteger(message, 0,120);
         return userAgeInput;
     }
 
-    private String[] colorsAsStringArray(){
-        String[] colorsAsStringArray = new String[this.colors.size()];
-        for (int i = 0; i <colorsAsStringArray.length ; i++) {
-            String colorString;
-            switch(this.colors.get(i).toString()){
-                case "java.awt.Color[r=0,g=0,b=255]":
-                    colorString = "blue";
-                    break;
-                case "java.awt.Color[r=255,g=200,b=0]":
-                    colorString = "orange";
-                    break;
-                case "java.awt.Color[r=255,g=255,b=255]":
-                    colorString = "white";
-                    break;
-                case "java.awt.Color[r=0,g=0,b=0]":
-                    colorString = "black";
-                    break;
-                default:
-                    colorString = "gray";
-            }
-            colorsAsStringArray[i]= colorString;
-        }
-        return colorsAsStringArray;
-    }
-
-    private Color stringToColor(String colorString){
-        switch(colorString.toLowerCase()){
-            case "blue":
-                return Color.BLUE;
-            case "orange":
-                return Color.ORANGE;
-            case "white":
-                return Color.WHITE;
-            case "black":
-                return Color.BLACK;
-            default:
-                return Color.GRAY;
-        }
-    }
-
-    private void removeColorFromChoices(Color color){
-        for(Color colorInList : this.colors){
-            if(colorInList.toString().equals(color.toString())){
-                this.colors.remove(colorInList);
-                break;
-            }
-        }
-    }
-
-    private String getUserChoice(String message, String... options){
+    public String getUserChoice(String message, String... options){
         String userChoice = this.gui.getUserSelection(message, options);
         return userChoice;
     }
