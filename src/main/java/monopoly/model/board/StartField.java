debcce1b8dbd443e711232c05ecc4ab79830bfd5
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-19 16:40:21 +0100

    Added StartField ( no implementation )

---
+++ src/main/java/monopoly/model/board/StartField.java
@@ -0,0 +1,18 @@
+package monopoly.model.board;
+
+import monopoly.model.Player;
+
+import java.awt.*;
+
+public class StartField extends Field{
+
+    public StartField(String title, String subtitle, String description, String message, Color color) {
+        super(title, subtitle, description, message, color);
+    }
+
+    @Override
+    public void resolveEffect(Player player) {
+
+    }
+
+}
