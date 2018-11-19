a1146bd1d5b09a9faa8486dc89cca763c04c18c1
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-19 16:32:19 +0100

    Added JailField

---
+++ src/main/java/monopoly/model/board/JailField.java
@@ -0,0 +1,16 @@
+package monopoly.model.board;
+
+import monopoly.model.Player;
+
+import java.awt.*;
+
+public class JailField extends Field {
+
+    public JailField(String title, String subtitle, String description, String message, Color color){
+        super(title, subtitle, description, message, color);
+    }
+
+    @Override
+    public void resolveEffect(Player player) {
+    }
+}
