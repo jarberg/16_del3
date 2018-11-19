25b907743ae55617cbd2a49b34dcbc102fc90097
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-19 16:28:48 +0100

    Added PropertyField implementation

--- src/main/java/monopoly/model/board/Field.java
+++ src/main/java/monopoly/model/board/Field.java
@@ -1,34 +1,28 @@
-package monopoly.model;
+package monopoly.model.board;
+
+import monopoly.model.Player;
 
 import java.awt.*;
 
 public abstract class Field {
 
-    private Player owner;
     private String title;
     private String subtitle;
     private String description;
     private String message;
     private Color color;
     private Color borderColor;
-    private boolean buyable;
-    private int value;
 
-    public Field(String title, String subtitle, String description, String message, Color color, int value, Player player){
+    public Field(String title, String subtitle, String description, String message, Color color){
         this.title = title;
         this.subtitle = subtitle;
         this.message = message;
         this.color = color;
-        this.value = value;
         this.description = description;
         this.borderColor = Color.black;
-        this.buyable = false;
-        this.owner = player;
     }
 
-    public boolean isOwner(Player player){
-        return this.owner.equals(player);
-    }
+    public abstract void resolveEffect(Player player);
 
 }
 
