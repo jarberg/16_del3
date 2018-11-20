a1094bc35f090b150d627cd699a9cb6c3f23de17
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-20 14:12:39 +0100

    Setup initial methods

--- src/main/java/monopoly/controller/GameController.java
+++ src/main/java/monopoly/controller/GameController.java
@@ -1,4 +1,24 @@
 package monopoly.controller;
 
+import monopoly.model.board.Board;
+import monopoly.view.MonopolyView;
+
 public class GameController {
+
+    private MonopolyView view;
+    private Board board;
+
+    public GameController(){
+        this.view = new MonopolyView();
+    }
+
+    public void setupGame(){
+
+    }
+
+    public void playGame(){
+
+    }
+
+
 }
