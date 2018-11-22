b853832cb934688576daf41a7d0716c5322b3128
jdkramhoft <s175445@student.dtu.dk>
2018-11-22 15:20:15 +0100

    Finalizing for merge

--- src/main/java/monopoly/controller/GameController.java
+++ src/main/java/monopoly/controller/GameController.java
@@ -5,31 +5,58 @@ import monopoly.view.MonopolyView;
 
 public class GameController {
 
-    private MonopolyView view;
+    private ViewController viewController;
     private MonopolyFileReader fileReader;
     private Board board ;
-    private String filepath = "TextFiles/";
+    private String languageFilepath;
     private static final String defaultLanguage = "English";
+    private int playerAmount = 0;
 
     public GameController(){
+        viewController = new ViewController();
         setFilepathLanguage(defaultLanguage);
-        view = new MonopolyView();
-
     }
 
     public void setupGame(){
         setupLanguage();
+        //createGameBoard();
+        //playerAmount = getPlayerAmount();
+        //createAndAddPlayers();
+        //showGameBoard();
     }
 
     private void setupLanguage(){
-        view.showEmptyGUI();
-        String[] languageChoices = fileReader.getDirectoriesStringArray();
-        String userLanguage = view.getUserLanguage(languageChoices);
+        viewController.showEmptyGUI();
+        String userLanguage = viewController.getUserLanguage();
         setFilepathLanguage(userLanguage);
     }
 
+    /*
+    private void createGameBoard(){
+
+
+    }
+    */
+
+    /*
+    private int getPlayerAmount(){
+        if(playerAmount == 0){
+            playerAmount = viewController.getPlayerAmount();
+        }
+        return playerAmount;
+        //int playerAmount = viewController.getPlayerAmount();
+    }
+    */
+
+    /*
+    private void createAndAddPlayers(){
+
+    }
+     */
+
     private void setFilepathLanguage(String language){
-        filepath = "TextFiles/" + language;
+        languageFilepath = "TextFiles/" + language;
+        viewController.setFilepath(languageFilepath);
     }
 
 
--- src/main/java/monopoly/controller/ViewController.java
+++ src/main/java/monopoly/controller/ViewController.java
@@ -1,4 +1,30 @@
 package monopoly.controller;
 
+import monopoly.view.MonopolyView;
+
+import java.io.FileReader;
+
 public class ViewController {
+
+    private MonopolyFileReader fileReader;
+    private MonopolyView view;
+    private String filepath;
+
+    public ViewController(){
+        this.fileReader = new MonopolyFileReader();
+    }
+
+    public String getUserLanguage(){
+        String[] languageChoices = fileReader.getDirectoriesStringArray();
+        return view.getUserLanguage(languageChoices);
+    }
+
+    public void showEmptyGUI(){
+        view.showEmptyGUI();
+    }
+
+    public void setFilepath(String filepath){
+        this.filepath = filepath;
+    }
+
 }
