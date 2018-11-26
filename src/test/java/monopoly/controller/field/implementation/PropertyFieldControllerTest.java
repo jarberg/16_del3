f67c5da9420506918a59db8f8bf638ae32095f0b
jarberg <mjarberg88@gmail.com>
2018-11-26 15:44:34 +0100

    updated old english files

---
+++ src/test/java/monopoly/controller/field/implementation/PropertyFieldControllerTest.java
@@ -0,0 +1,23 @@
+package monopoly.controller.field.implementation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PropertyFieldControllerTest {
+
+    PropertyFieldController propCon= new PropertyFieldController();
+
+    @Test
+    public void resolveEffect() {
+        //propCon.resolveEffect(, );
+    }
+
+    @Test
+    public void checkIfPair() {
+    }
+
+    @Test
+    public void getFieldIndex() {
+    }
+}
